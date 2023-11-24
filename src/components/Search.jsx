07d2790ebdc940c5f706cf94f40a0e8356399d29
Ratul <ib.ratul008gmail.com>
2023-11-25 01:28:18 +0600

    updated Search

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,7 +1,22 @@
 import React from "react";
 
 const Search = () => {
-  return <div></div>;
+  return (
+    <div className="search">
+      <div className="searchForm">
+        <input type="text" placeholder="find a user" />
+      </div>
+      <div className="userChat">
+        <img
+          src="https://images.unsplash.com/photo-1534528741775-53994a69daeb?q=80&w=1064&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          alt=""
+        ></img>
+        <div className="userChatInfo">
+          <span>Jane</span>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Search;
